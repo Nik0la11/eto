d36f9d10e3458e8c7f34bf0009ac96aee14f1fb3
Nik0la11 <marcetanikola3@gmail.com>
2025-06-22 19:52:51 +0200

    added backend for my appoints

--- src/app/components/MyAppoints.jsx
+++ src/app/components/MyAppoints.jsx
@@ -4,8 +4,13 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 
 import { useRouter } from "next/navigation";
 import { UserIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
 
 const MyAppoints = ({ appointment, setAppointment }) => {
+  const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+  const [token, setToken] = useState(null);
+  const [data, setData] = useState(null);
+
   const handleAppointment = () => {
     setAppointment(true);
   };
@@ -15,6 +20,27 @@ const MyAppoints = ({ appointment, setAppointment }) => {
   const handleSignIn = () => {
     route.push("/signIn");
   };
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+
+    setToken(storedToken);
+  }, []);
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch(`${BASE_URL}/v1/appointment/my`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch((err) => console.log("Error fetching data:", err));
+  }, [token]);
 
   return (
     <div
@@ -38,63 +64,89 @@ const MyAppoints = ({ appointment, setAppointment }) => {
               poruke koju ste primili ili telefonskim pozivom!
             </p>
 
-            <div className="flex justify-center items-center gap-2 w-full">
-              <div className="h-12 grow bg-[#E5E4DF] flex justify-center items-center ">
-                <p className="font-['Montserrat'] font-semibold">#</p>
+            <div className="grid grid-cols-4 gap-2">
+              <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                #
               </div>
-              <div className="h-12 grow bg-[#E5E4DF]  flex justify-center items-center ">
-                <p className="font-['Montserrat'] font-semibold">Datum</p>
+              <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                Datum
               </div>
-              <div className="h-12 grow bg-[#E5E4DF]  flex justify-center items-center">
-                <p className="font-['Montserrat'] font-semibold">Vreme</p>
+              <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                Vreme
               </div>
-              <div className="h-12 grow bg-[#E5E4DF]  flex justify-center items-center ">
-                <p className="font-['Montserrat'] font-semibold">Status</p>
+              <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                Status
+              </div>
+
+              {data?.data?.map((appointment) => (
+                <>
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    {appointment.id}
+                  </div>
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    {new Date(appointment.start_time).toLocaleDateString(
+                      "en-CA"
+                    )}
+                  </div>
+
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    {new Date(appointment.start_time).toLocaleTimeString(
+                      "en-GB",
+                      {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      }
+                    )}
+                  </div>
+
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    Status
+                  </div>
+                </>
+              ))}
+            </div>
+
+            <div>
+              <div className="mt-8 mb-4">
+                <div className="flex mb-2">
+                  <h1 className="uppercase font-bold text-[#D4AF37] text-3xl flex-1">
+                    Lista otkazanih termina
+                  </h1>
+                </div>
+
+                <div className="grid grid-cols-4 gap-2">
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    #
+                  </div>
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    Datum
+                  </div>
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    Vreme
+                  </div>
+                  <div className="h-12 grow bg-[#E5E4DF] font-['Montserrat'] font-semibold flex justify-center items-center">
+                    Status
+                  </div>
+                </div>
+
+                <div className="my-2"></div>
+              </div>
+              <div className="flex justify-end gap-2">
+                {
+                  <Button
+                    onClick={handleSignIn}
+                    className="place-self-end flex gap-2"
+                  >
+                    <UserIcon className="h-6 w-6 text-black-600" />
+                    <p>Prijavi se</p>
+                  </Button>
+                }
+                <Button onClick={handleAppointment} className="place-self-end">
+                  Zatvori
+                </Button>
               </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div className="mt-8 mb-4">
-            <div className="flex mb-2">
-              <h1 className="uppercase font-bold text-[#D4AF37] text-3xl flex-1">
-                Lista otkazanih termina
-              </h1>
-            </div>
-
-            <div className="mb-2">
-              <div className="flex justify-center items-center gap-2 w-full">
-                <div className="h-12 grow bg-[#E5E4DF] flex justify-center items-center ">
-                  <p className="font-['Montserrat'] font-semibold">#</p>
-                </div>
-                <div className="h-12 grow bg-[#E5E4DF]  flex justify-center items-center ">
-                  <p className="font-['Montserrat'] font-semibold">Datum</p>
-                </div>
-                <div className="h-12 grow bg-[#E5E4DF]  flex justify-center items-center">
-                  <p className="font-['Montserrat'] font-semibold">Vreme</p>
-                </div>
-                <div className="h-12 grow bg-[#E5E4DF]  flex justify-center items-center ">
-                  <p className="font-['Montserrat'] font-semibold">Status</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="my-2"></div>
-        </div>
-        <div className="flex justify-end gap-2">
-          {
-            <Button
-              onClick={handleSignIn}
-              className="place-self-end flex gap-2"
-            >
-              <UserIcon className="h-6 w-6 text-black-600" />
-              <p>Prijavi se</p>
-            </Button>
-          }
-          <Button onClick={handleAppointment} className="place-self-end">
-            Zatvori
-          </Button>
         </div>
       </div>
     </div>
