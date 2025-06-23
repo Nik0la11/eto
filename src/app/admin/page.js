2c95e86ae53abfc78d114c005b8480c5e74cdb3a
Nik0la11 <marcetanikola3@gmail.com>
2025-06-23 18:19:56 +0200

    created admin panel

---
+++ src/app/admin/page.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import AdminContent from "./components/AdminContent";
+import AddAppointment from "./components/AddAppointment";
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="flex w-full h-screen ">
+        <SideBar />
+        <AdminContent />
+      </div>
+      <AddAppointment />
+    </div>
+  );
+}
